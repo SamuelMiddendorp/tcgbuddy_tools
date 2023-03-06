904ef6d0595f051b8ea5744c817c562e0dafdcbe
Samuel Middendorp <samiezaurus@gmail.com>
2023-03-06 13:28:16 +0100

    Delegate error handling to deticated functions

--- CardDataTransformer/main.go
+++ CardDataTransformer/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
@@ -36,24 +37,36 @@ func GetAndWrite(url string, filename string, apiKey string) error {
 	os.WriteFile("out/"+filename+".json", body, 0644)
 	return err
 }
+func succeedOrFatal(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func readOrFatal(path string) []byte {
+	fileContent, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return fileContent
+}
 func main() {
-	fileContent, err := os.ReadFile("inSets.json")
-	if err != nil {
-	}
+
+	inSetsContent := readOrFatal("inSets.json")
+
 	var dataSets []string
-	err = json.Unmarshal(fileContent, &dataSets)
-	if err != nil {
-	}
-	inSetAmounts, err := os.ReadFile("inSetAmounts.json")
-	if err != nil {
-	}
+	succeedOrFatal(json.Unmarshal(inSetsContent, &dataSets))
+
+	inSetsAmountContent := readOrFatal("inSetAmounts.json")
+
 	var dataSetAmounts []int
-	err = json.Unmarshal(inSetAmounts, &dataSetAmounts)
-	if err != nil {
-	}
+	succeedOrFatal(json.Unmarshal(inSetsAmountContent, &dataSetAmounts))
+
 	for i, set := range dataSets {
 		GetAndWrite(fmt.Sprintf("https://api.pokemontcg.io/v2/cards?q=set.id:%s&page=1&pageSize=250", set), set, os.Args[1])
-		if dataSetAmounts[i] > 250 {
+
+		// Some modern sets are bigger than the max allowed pagesize of the backing API
+		if dataSetAmounts[i] > 249 {
 			GetAndWrite(fmt.Sprintf("https://api.pokemontcg.io/v2/cards?q=set.id:%s&page=2&pageSize=250", set), os.Args[1], set+"_ext")
 		}
 
